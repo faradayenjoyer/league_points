a14a694be92bc24c8e8e1aa54145c65b1b21ce94
faradayenjoyer <120593155+faradayenjoyer@users.noreply.github.com>
2023-02-16 15:46:02 -0600

    too ez
    im just like that

--- solution.py
+++ solution.py
@@ -1,7 +1,10 @@
-def league_points(wins, draws, losses):
-    ...
-
-
-
-
-    
+def abc():
+    num = input("Tell me the wins, and draws seperate with commas (in that order, wins -> draws), no spaces. ").split(",")
+    num = list(map(int, num))
+    wins = num[0]
+    draws = num[1]
+    wins *= 2
+    draws *= 1
+    total = wins + draws
+    print(total)
+abc()
